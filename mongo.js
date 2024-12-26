d6f6b9083f1c398dc535c41588fc41972db826fc
Daniel-Ibay <danielibay1538@outlook.com>
2024-12-26 17:04:48 +0100

    No funciona

--- mongo.js
+++ mongo.js
@@ -12,11 +12,17 @@ const password = process.argv[2]
 const name = process.argv[3]
 const number = process.argv[4]
 
-const url = `mongodb+srv://daniel-ibay:${password}@phonebook-test.dx0j7.mongodb.net/?retryWrites=true&w=majority&appName=phonebook-test`
+const url = `mongodb+srv://daniel-ibay:melvin1538@phonebook-test.dx0j7.mongodb.net/?retryWrites=true&w=majority&appName=phonebook-test`
 
 mongoose.set('strictQuery', false)
 
 mongoose.connect(url)
+.then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 if (process.argv[4]) {
 
